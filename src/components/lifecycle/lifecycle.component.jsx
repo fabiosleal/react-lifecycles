4dbce50b9315a05673ef440e2c4c65cc92bcf53a
Fabio Leal <fabioleal@Fabios-Mac-mini.local>
2020-05-18 01:47:56 -0300

    only an adjustment

--- src/components/lifecycle/lifecycle.component.jsx
+++ src/components/lifecycle/lifecycle.component.jsx
@@ -10,19 +10,19 @@ class LifeCycles extends React.Component {
         console.log("componentDidMount!");
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("shouldComponentUpdate!", nextProps);
+        // If the porps comming in is not different than the props we already have, there is no reason to rerender
+        // This is mainly for performance.
+        return this.props.text !== nextProps.text;
+    }
+
     componentDidUpdate() {
         console.log("componentDidUpdate!");
     }
 
     componentWillUnmount() {
         console.log("componentWillUnmount!");
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("shouldComponentUpdate!", nextProps);
-        // If the porps comming in is not different than the props we already have, there is no reason to rerender
-        // This is mainly for performance.
-        return this.props.text !== nextProps.text;
     }
 
     render() {
